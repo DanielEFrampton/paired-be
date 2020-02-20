0d33be71d1a220a2720e0d5854133a26e4613205
Carleigh Crockett <capleugh@gmail.com>
2020-02-20 15:42:52 -0700

    Change users to get_users
    Co-Authored-By: Becky Robran <52762517+rer7891@users.noreply.github.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    field :users, [Types::UserType], null: false, description: 'Returns a list of users'
+    field :get_users, [Types::UserType], null: false, description: 'Returns a list of users'
     field :skills, [Types::SkillType], null: false, description: 'Returns a list of skills'
+    field :get_user, Types::UserType, null: false, description: 'Returns a single user by id' do
+      argument :id, ID, required: true
+    end
 
-    def users
+    def get_users
       User.all
+    end
+
+    def get_user(id:)
+      User.find(id)
     end
 
     def skills
