f08e5cb6721e7f8413a360a2deb3fbbd9d766b81
Becky Robran <rerobran@gmail.com>
2020-02-20 12:48:07 -0700

    creates model test for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+   describe 'validations' do
+      it { should validate_presence_of :name }
+      it { should validate_presence_of :mod }
+      it { should validate_presence_of :program }
+      it { should validate_presence_of :pronouns }
+      it { should validate_presence_of :slack }
+      it { should validate_presence_of :image }
+      it { should validate_presence_of :phone_number }
+    end
+
+    describe 'relationships' do
+      it { should have_many :interests }
+      it { should have_many :skills }
+      it { should have_many :paired_users }
+      it { should have_many(:pairees).through(:paired_users) }
+      it { should have_many :pairing_users }
+      it { should have_many(:pairers).through(:pairing_users) }
+    end
+end
