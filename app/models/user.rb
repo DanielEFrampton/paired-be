2f3b05b50cfe9faab3c990d6bc44eec3f7e32333
Becky Robran <rerobran@gmail.com>
2020-02-20 12:40:31 -0700

    creates model user

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  validates_presence_of :name, :mod, :program, :pronouns,
+                        :slack, :email, :image, :phone_number
+
+  has_many :skills, dependent: :destroy
+  has_many :interests, dependent: :destroy
+
+  has_many :paired_users, foreign_key: :pairer_id, class_name: 'Pairing'
+  has_many :pairees, through: :paired_users
+
+  has_many :pairing_users, foreign_key: :pairee_id, class_name: 'Pairing'
+  has_many :pairers, through: :pairing_users
+end
