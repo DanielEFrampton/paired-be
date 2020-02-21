b77cf9becfb7c4e88d24cf254352bb6cf48cd0be
Becky Robran <rerobran@gmail.com>
2020-02-20 20:01:33 -0700

    updates query types pairing field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,7 +2,8 @@ module Types
   class QueryType < Types::BaseObject
     field :get_users, [Types::UserType], null: false, description: 'Returns a list of users'
     field :skills, [Types::SkillType], null: false, description: 'Returns a list of skills'
-    field :get_pairings, Types::PairingType, null: false, description: 'Returns all pairings'
+
+    field :get_pairings, [Types::PairingType], null: false, description: 'Returns all pairings'
     field :get_user, Types::UserType, null: false, description: 'Returns a single user by id' do
       argument :id, ID, required: true
     end
