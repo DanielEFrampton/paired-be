e6e8f9f47d1ac8c2641a76a392123b053daff6c1
Carleigh Crockett <capleugh@gmail.com>
2020-02-21 15:51:20 -0700

    Build get_pairing endpoint which returns a single pairing
    Co-authored-by: Becky Robran <52762517+rer7891@users.noreply.github.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,11 +2,16 @@ module Types
   class QueryType < Types::BaseObject
     field :get_users, [Types::UserType], null: false, description: 'Returns a list of users'
     field :skills, [Types::SkillType], null: false, description: 'Returns a list of skills'
-
     field :get_pairings, [Types::PairingType], null: false, description: 'Returns all pairings'
+
     field :get_user, Types::UserType, null: false, description: 'Returns a single user by id' do
       argument :id, ID, required: true
     end
+
+    field :get_pairing, Types::PairingType, null: false, description: 'Returns a single pairing by id' do
+      argument :id, ID, required: true
+    end
+
 
     def get_users
       User.all
@@ -18,6 +23,11 @@ module Types
 
     def get_pairings
       Pairing.all
+    end
+
+    def get_pairing(id:)
+      # require "pry"; binding.pry
+      Pairing.find(id)
     end
 
     def skills
