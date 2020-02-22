14fd68c443799923351c3b92b9ce4add73e991ee
Becky Robran <rerobran@gmail.com>
2020-02-22 14:13:44 -0700

    updates create user file

--- app/graphql/mutations/users/create_user.rb
+++ app/graphql/mutations/users/create_user.rb
@@ -1,60 +1,16 @@
-# module Mutations
-#   module Users
-#     class CreateUser < ::Mutations::BaseMutation
-#
-#     field :create_user, Types::UserType, null: true do
-#        argument :name, String, required: true
-#        argument :mod, String, required: true
-#        argument :program, String, required: true
-#        argument :pronouns, String, required: true
-#        argument :slack, String, required: false
-#        argument :email, String, required: true
-#        argument :image, String, required: false
-#        argument :phone_number, String, required: true
-#
-#      resolve ->(_obj, args, _ctx) do
-#        User.create(
-#          name: args[:name],
-#          mod: args[:mod],
-#          program: args[:program],
-#          email: args[:email],
-#          phone_number: args[:phone_number],
-#        )
-#        end
-#      end
-#    end
-#   end
-# end
+module Mutations
+  module Users
+    class CreateUser < ::Mutations::BaseMutation
+      argument :name, String, required: true
+      argument :program, String, required: true
+      argument :mod, String, required: true
 
-class Mutations::Users::CreateUser < Mutations::BaseMutation
-  null true
+      type Types::UserType
 
-  argument :name, String, required: true
-  argument :mod, String, required: true
-  argument :program, String, required: true
-  # argument :pronouns, String, required: true
-  # argument :slack, String, required: false
-  # argument :email, String, required: true
-  # argument :image, String, required: false
-  # argument :phone_number, String, required: true
-
-  field :user, Types::UserType, null: true
-  field :errors, [String], null: false
-
-  def resolve(name:, mod:, program:)
-    user = User.create(name: body, mod: mod, program: program)
-    if user.save
-      # Successful creation, return the created object with no errors
-      {
-        user: user,
-        errors: [],
-      }
-    else
-      # Failed save, return the errors to the client
-      {
-        user: nil,
-        errors: comment.errors.full_messages
-      }
+      def resolve(attributes)
+require "pry"; binding.pry
+        User.create(attributes)
+      end
     end
   end
 end
