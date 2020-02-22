05cdc08788332ad952c6c970bed9c42a542dcc55
Becky Robran <rerobran@gmail.com>
2020-02-22 14:50:14 -0700

    updates schema

--- app/graphql/paired_be_schema.rb
+++ app/graphql/paired_be_schema.rb
@@ -8,4 +8,14 @@ class PairedBeSchema < GraphQL::Schema
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
+
+#   input MutationInput {
+#    name: String
+#    mod: String
+#    program: String
+# }
+#
+# type Mutation {
+#   createUser(input: MutationInput): String
+# }
 end
