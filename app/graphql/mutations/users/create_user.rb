f30ae10e0aab6e91cd00c000fbc3a30aa6c3ee3a
Becky Robran <rerobran@gmail.com>
2020-02-22 14:50:02 -0700

    updates muations users

--- app/graphql/mutations/users/create_user.rb
+++ app/graphql/mutations/users/create_user.rb
@@ -1,9 +1,15 @@
 module Mutations
   module Users
     class CreateUser < ::Mutations::BaseMutation
+      argument :user, Types::UserType, required: true
       argument :name, String, required: true
-      argument :program, String, required: true
+      argument :email, String, required: true
+      argument :image, String, required: true
       argument :mod, String, required: true
+      argument :program, String, required: true
+      argument :pronouns, String, required: true
+      argument :slack, String, required: true
+      argument :phone_number, String, required: true
 
       type Types::UserType
 
