fd867ba8f1a846d0c57102544127fad9a5cef6a7
Becky Robran <rerobran@gmail.com>
2020-02-23 12:42:40 -0700

    creates delete user pairings endpoint

---
+++ app/graphql/mutations/pairings/delete_user_pairings.rb
@@ -0,0 +1,15 @@
+module Mutations
+  module Pairings
+    class DeleteUserPairings < ::Mutations::BaseMutation
+        argument :id, ID, required: true
+
+      type Types::PairingType
+
+      def resolve(attributes)
+      id = attributes[:id]
+      userpairings = Pairing.where(pairer_id: id)
+      userpairings.destroy_all
+      end
+    end
+  end
+end
