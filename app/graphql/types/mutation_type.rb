a1d8403fa0487fe97b2b7c64878969f64283719e
msimon42 <msimon@uab.edu>
2020-02-23 15:08:00 -0700

    Rename to DeletePairing

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::Users::CreateUser
     field :create_pairings, mutation: Mutations::Pairings::CreatePairings
+    field :delete_pairing, mutation: Mutations::Pairings::DeletePairing
   end
 end
