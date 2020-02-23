e35541d435bae7abd0a613fd7948f8969a9c2fb8
msimon42 <msimon@uab.edu>
2020-02-22 21:12:54 -0700

    Add CreatePairing mutation

---
+++ app/graphql/mutations/pairings/create_pairings.rb
@@ -0,0 +1,17 @@
+module Mutations
+  module Pairings
+    class CreatePairing < Mutations::BaseMutation
+      argument :pairer_id, Integer, required: true
+      argument :pairee_id, Integer, required: true
+      argument :date, String, required: true
+      argument :time, String, required: true
+      argument :notes, String, required: false
+
+      type Types::PairingType
+
+      def resolve(attributes)
+        Pairing.create(attributes)
+      end
+    end
+  end
+end
