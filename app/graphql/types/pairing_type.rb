2a16fa1a846c5d519c777d2dc9c5cf239bcea81d
Becky Robran <rerobran@gmail.com>
2020-02-23 12:43:02 -0700

    adds deletePairings field in mutations

--- app/graphql/types/pairing_type.rb
+++ app/graphql/types/pairing_type.rb
@@ -1,17 +1,19 @@
 module Types
   class PairingType < Types::BaseObject
     field :id, ID, null: false
-    field :pairer, Types::UserType, null: false
-    field :pairee, Types::UserType, null: false
+    field :pairer, Types::UserType, null: true
+    field :pairee, Types::UserType, null: true
     field :date, String, null: true
     field :time, String, null: true
     field :notes, String, null: true
 
     def pairer
+      return if object.empty?
       User.find(object.pairer_id)
     end
 
     def pairee
+      return if object.empty?
       User.find(object.pairee_id)
     end
   end
