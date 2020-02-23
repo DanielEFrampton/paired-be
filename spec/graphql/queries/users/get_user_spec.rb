60941473e836a46f718f098b75696f65dd21f00a
Becky Robran <rerobran@gmail.com>
2020-02-22 21:02:27 -0700

    adds get user spec

---
+++ spec/graphql/queries/users/get_user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe 'display users' do
+    it 'can query all users' do
+      create(:user, name: "Bruce Easley")
+      create_list(:user, 4)
+
+      result = PairedBeSchema.execute(query).as_json
+      expect(result["data"]["getUsers"].count).to eq(5)
+      expect(result["data"]["getUsers"].first["name"]).to eq("Bruce Easley")
+
+      users = User.all
+      expect(result.dig("data", "getUsers")).to match_array(
+      users.map { |user| {"mod" => user.mod, "name" => user.name, "program" => user.program} })
+    end
+  end
+
+  def query
+    <<~GQL
+    {
+    getUsers {
+      name
+      program
+      mod
+      }
+    }
+    GQL
+end
+end
