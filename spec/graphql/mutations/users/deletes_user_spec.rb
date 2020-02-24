1a758ece78b1415f63fc04f034da6668a8a492e6
Becky Robran <rerobran@gmail.com>
2020-02-23 21:06:28 -0700

    fixes indentation

--- spec/graphql/mutations/users/deletes_user_spec.rb
+++ spec/graphql/mutations/users/deletes_user_spec.rb
@@ -3,14 +3,14 @@ module Mutations
   module Users
 RSpec.describe DeleteUser, type: :request do
   describe '.resolve' do
-        it 'deletes a user' do
-          create_list(:user, 3)
-          create(:user, id: 1)
-          users = User.all
-          expect(User.count).to eq(4)
-          post '/graphql', params: {query: query}
-          expect(User.count).to eq(3)
-        end
+    it 'deletes a user' do
+      create_list(:user, 3)
+      create(:user, id: 1)
+      users = User.all
+      expect(User.count).to eq(4)
+      post '/graphql', params: {query: query}
+      expect(User.count).to eq(3)
+    end
 
     it 'returns id of deleted User after user deletion' do
       user = create(:user, id: 1)
@@ -45,12 +45,12 @@ RSpec.describe DeleteUser, type: :request do
     def query
       <<~GQL
       mutation {
-     deleteUser(
+        deleteUser(
         	input:{
             id: "1" } ) {
           name
+        }
       }
-    }
       GQL
     end
 
