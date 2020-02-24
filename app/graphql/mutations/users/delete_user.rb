45bbd870cdecd9eb48221b031e30c882417be436
Becky Robran <rerobran@gmail.com>
2020-02-23 20:55:31 -0700

    updates delete user mutation

--- app/graphql/mutations/users/delete_user.rb
+++ app/graphql/mutations/users/delete_user.rb
@@ -7,8 +7,15 @@ module Mutations
 
       def resolve(attributes)
         id = attributes[:id]
+        delete_pairings(id)
+
         user = User.find(id)
         user.destroy
+      end
+private
+      def delete_pairings(id)
+        pairings = Pairing.where('pairer_id = ? AND pairee_id IS NULL', id)
+        pairings.destroy_all if pairings.any?
       end
     end
   end
