e32dda5628e5ea67056f12ac8fb7221191088013
Becky Robran <rerobran@gmail.com>
2020-02-23 21:01:33 -0700

    indents method

--- app/graphql/mutations/pairings/delete_user_pairings.rb
+++ app/graphql/mutations/pairings/delete_user_pairings.rb
@@ -6,9 +6,9 @@ module Mutations
       type Types::PairingType
 
       def resolve(attributes)
-      id = attributes[:id]
-      userpairings = Pairing.where(pairer_id: id)
-      userpairings.destroy_all
+        id = attributes[:id]
+        userpairings = Pairing.where(pairer_id: id)
+        userpairings.destroy_all
       end
     end
   end
