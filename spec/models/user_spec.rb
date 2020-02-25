7ba41c10299a8e39e9d9f76bd9368f1484754939
Becky Robran <rerobran@gmail.com>
2020-02-25 14:57:39 -0700

    takes space out
    Co-Authored-By: Carleigh Crockett <Capleugh@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe User, type: :model do
       it { should validate_length_of(:phone_number).is_equal_to(10) }
       it { should validate_numericality_of(:mod).is_greater_than_or_equal_to(1) }
       it { should validate_numericality_of(:mod).is_less_than_or_equal_to(5) }
-
     end
 
     describe 'relationships' do
