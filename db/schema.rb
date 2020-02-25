7fa6ff4b87cd3b371fa5a93810621acab2b895e3
Becky Robran <rerobran@gmail.com>
2020-02-25 14:12:44 -0700

    changes mod type from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_210547) do
+ActiveRecord::Schema.define(version: 2020_02_25_205645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 2020_02_22_210547) do
     t.string "phone_number", limit: 10
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "mod"
     t.string "firebase_id"
+    t.integer "mod"
   end
 
   add_foreign_key "interests", "users"
