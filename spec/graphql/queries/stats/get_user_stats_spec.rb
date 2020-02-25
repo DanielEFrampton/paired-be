161c50eda00d8bee3a32e6564c6f2d45f9ccc6f7
Becky Robran <rerobran@gmail.com>
2020-02-25 12:05:08 -0700

    updates user stats spec to include json output validations

--- spec/graphql/queries/stats/get_user_stats_spec.rb
+++ spec/graphql/queries/stats/get_user_stats_spec.rb
@@ -3,21 +3,25 @@ require 'rails_helper'
 RSpec.describe Types::QueryType do
   describe 'display users' do
     before :each do
-      user = create(:user, id: 1)
-      create_list(:user, 20)
-      5.times do
-        count = User.count - 1
-        user_1 = User.offset(rand(0..count)).limit(1).first
-        user_2 = User.offset(rand(0..count)).limit(1).first
+      user = create(:user, id: 1, name: 'Susie Q')
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
 
-        create(:pairing, pairer_id: user.id, pairee_id: user_1.id)
-        create(:pairing, pairer_id: user_1.id, pairee_id: user_2.id)
-        create(:pairing, pairer_id: user_2.id, pairee_id: user_1.id)
-      end
+      create(:pairing, pairer_id: user.id, pairee_id: user_1.id)
+      create(:pairing, pairer_id: user.id, pairee_id: user_2.id)
+      create(:pairing, pairer_id: user.id, pairee_id: user_3.id)
+      create(:pairing, pairer_id: user.id, pairee_id: user_3.id)
+      create(:pairing, pairer_id: user.id, pairee_id: user_3.id)
     end
     it 'can find user stats' do
       result = PairedBeSchema.execute(query).as_json
-    end
+      expect(result["data"]["getUserStats"]["name"]).to eq("Susie Q")
+      expect(result["data"]["getUserStats"]["totalBookings"]).to eq(5)
+      expect(result["data"]["getUserStats"]["totalMentorHours"]).to eq(2.5)
+      expect(result["data"]["getUserStats"]["totalHoursMentored"]).to eq(0)
+      expect(result["data"]["getUserStats"]["mentees"].count).to eq(3)
+    end 
   end
 
   def query
