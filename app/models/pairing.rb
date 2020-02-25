278d99c14ed7f86c8e30592271c14220a401156a
msimon42 <msimon@uab.edu>
2020-02-25 15:21:26 -0700

    Add methods

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -1,4 +1,20 @@
 class Pairing < ApplicationRecord
   belongs_to :pairer, class_name: 'User'
   belongs_to :pairee, class_name: 'User', optional: true
+
+  def pairer_email
+    pairer.email
+  end
+
+  def pairee_email
+    pairee.email
+  end
+
+  def pairer_name
+    pairer.name
+  end
+
+  def pairee_name
+    pairee.name
+  end         
 end
