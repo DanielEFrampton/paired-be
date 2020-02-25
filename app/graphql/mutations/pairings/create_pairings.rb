25ec5a53e9f72b590a1427582a178379dab4271e
msimon42 <msimon@uab.edu>
2020-02-25 00:20:16 -0700

    Update mutation to fit fe query

--- app/graphql/mutations/pairings/create_pairings.rb
+++ app/graphql/mutations/pairings/create_pairings.rb
@@ -2,10 +2,15 @@ module Mutations
   module Pairings
     class CreatePairings < ::Mutations::BaseMutation
       argument :pairings, [Types::PairingAttributesType], required: true
-      type Types::PairingType
+      type Types::UserType
+
       def resolve(attributes)
-        binding.pry
-        Pairing.create(attributes)
+        pairer = User.find_by(firebase_id: attributes[:pairings][0][:pairer])
+        pairings = attributes[:pairings].map do |pairing|
+          data = pairing.to_h
+          Pairing.create(pairer: pairer, date: data[:date], time: data[:time])
+        end
+        pairer
       end
     end
   end
