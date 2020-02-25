6a6f55fef353bcc2315e383cc14aa9dd80ba05fb
msimon42 <msimon@uab.edu>
2020-02-25 00:31:28 -0700

    Update test for createPairings

--- spec/graphql/mutations/pairings/create_pairing_spec.rb
+++ spec/graphql/mutations/pairings/create_pairing_spec.rb
@@ -3,40 +3,35 @@ require 'rails_helper'
 RSpec.describe 'CreatePairings', type: :request do
   describe '.resolve' do
     before :each do
-      @users = create_list :user, 2
+      @user = create :user, firebase_id: '5caa7eebfdebb8348e53a48e'
       @query = <<~GQL
-            mutation {
-                pairing: createPairings(
-                  input: {
-                    date: "October 22",
-                    pairerId: #{@users[0].id},
-                    paireeId: #{@users[1].id},
-                    time: "4:00"
-                  }
-                  ) {
-                      date
-                      pairer {name}
-                      time
-                    }
-
-                  }
+              mutation {
+                createPairings(input: {
+                              pairings: [{date:"Mon Apr 29 2019",time:"morning",pairer:"5caa7eebfdebb8348e53a48e"},{date:"Mon May 06 2019",time:"morning",pairer:"5caa7eebfdebb8348e53a48e"}]
+                              }) {
+                                unbookedPairings {
+                                  pairer {id}
+                                  date
+                                  time
+                                }
+                              }
+                            }
               GQL
     end
 
-    it 'creates a pairing' do
+    it 'creates pairings' do
       expect(Pairing.count).to eq(0)
       post '/graphql', params: {query: @query}
-      expect(Pairing.count).to eq(1)
+      expect(Pairing.count).to eq(2)
     end
 
-    it 'returns a pairing' do
+    it 'returns pairings' do
       post '/graphql', params: { query: @query }
       json = JSON.parse(response.body)
-      data = json['data']
+      data = json['data']['createPairings']['unbookedPairings']
 
-      expect(data['pairing']['date']).to eq(Pairing.last.date)
-      expect(data['pairing']['time']).to eq(Pairing.last.time)
-      expect(data['pairing']['pairer']['name']).to eq(@users[0].name)
+      expect(data[0]['date']).to eq("Mon Apr 29 2019")
+      expect(data[0]['time']).to eq("morning")
     end
   end
 
