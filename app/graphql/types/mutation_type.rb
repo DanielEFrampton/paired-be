57648ef76310371c219e6027ae471784d020a5d3
Carleigh Crockett <capleugh@gmail.com>
2020-02-24 17:05:16 -0700

    Add update_pairing field to mutation_type
    Co-authored-by: Becky Robran <52762517+rer7891@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::Users::CreateUser
     field :update_user, mutation: Mutations::Users::UpdateUser
+    field :update_pairing, mutation: Mutations::Pairings::UpdatePairing
     field :delete_pairings, mutation: Mutations::Pairings::DeleteUserPairings
     field :delete_user, mutation: Mutations::Users::DeleteUser
   end
