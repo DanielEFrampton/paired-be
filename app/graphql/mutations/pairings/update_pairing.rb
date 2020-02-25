1ed5f02baa7719277cdd512bf3e0988688aa5953
Carleigh Crockett <capleugh@gmail.com>
2020-02-24 17:05:16 -0700

    Create update_pairing resolver
    Co-authored-by: Becky Robran <52762517+rer7891@users.noreply.github.com>

--- app/graphql/mutations/pairings/update_pairing.rb
+++ app/graphql/mutations/pairings/update_pairing.rb
@@ -2,14 +2,35 @@ module Mutations
   module Pairings
     class UpdatePairing < ::Mutations::BaseMutation
       argument :id, ID, required: true
-      argument :pairee_id, Integer, required: true
+      argument :pairee, Integer, required: true
       argument :notes, String, required: false
 
       type Types::PairingType
 
       def resolve(attributes)
-        require "pry"; binding.pry
-        pairing = Pairing.find(id)
+        pairing = Pairing.where(id: attributes[:id]).first
+        pairing.update(pairee_id: attributes[:pairee], notes: attributes[:notes])
+        pairing
+        sms_service(pairing)
+      end
+
+    private
+
+      def create_message(pairing)
+        pairee = User.where(id: pairing.pairee_id).first
+        name = pairee.name
+        date = pairing.date
+        time = pairing.time
+        message = MessageGenerator.new
+        message.pairing_notification(name, date, time)
+      end
+
+      def sms_service(pairing)
+        message = create_message(pairing)
+        pairer = User.where(id: pairing.pairer_id).first
+        phone_number = pairer.phone_number
+        sms = SmsService.new
+        sms.send_sms(phone_number, message)
       end
     end
   end
