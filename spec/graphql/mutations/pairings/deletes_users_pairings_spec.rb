7560f7e4e7ed40ccbcb25d6e958f1e62043840ce
Becky Robran <rerobran@gmail.com>
2020-02-27 11:07:15 -0700

    updates delete user pairings spec

--- spec/graphql/mutations/pairings/deletes_users_pairings_spec.rb
+++ spec/graphql/mutations/pairings/deletes_users_pairings_spec.rb
@@ -4,14 +4,16 @@ module Mutations
 RSpec.describe DeleteUserPairings, type: :request do
   describe '.resolve' do
         it 'deletes a user pairings' do
-          create_list(:user, 3)
           create(:user, id: 122)
-          users = User.all
-          create(:pairing, pairer_id: 122,  pairee_id: users[3].id )
-          create(:pairing, pairer_id: users[1].id, pairee_id: users[2].id )
-          expect(Pairing.count).to eq(2)
+          create(:pairing, pairer_id: 122,  pairee_id: nil)
+          create(:pairing, pairer_id: 122,  pairee_id: nil)
+          create(:pairing, pairer_id: 122,  pairee_id: nil)
+          create(:pairing, pairer_id: 122,  pairee_id: nil)
+          create(:pairing, pairer_id: 122,  pairee_id: nil)
+
+          expect(Pairing.count).to eq(5)
           post '/graphql', params: {query: query}
-          expect(Pairing.count).to eq(1)
+          expect(Pairing.count).to eq(0)
         end
 
     it 'returns a nil for a pairer after deletion' do
