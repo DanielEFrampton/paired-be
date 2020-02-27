2dd7e5141b101b5f8d762dc8693c4df230db2d50
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-26 22:11:19 -0700

    Refactor rack-cors config to allow different origins in diff envs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Rack-CORS allowed origin in this environment
+  config.allowed_cors_origins = ["http://landslide-57f9a.firebaseapp.com", "http://paired-turing.firebaseapp.com"]
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins Rails.application.config.allowed_cors_origins
 
     resource '*',
       headers: :any,
