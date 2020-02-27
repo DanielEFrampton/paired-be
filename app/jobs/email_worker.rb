15127d66d3cda1156f21c7b461f168bafe29b9ac
msimon42 <msimon@uab.edu>
2020-02-26 17:47:12 -0700

    Merge workers

--- app/jobs/email_worker.rb
+++
@@ -1,8 +0,0 @@
-class EmailWorker < ActiveJob::Base
-  sidekiq_options retry: false
-  queue_as :default
-
-  def perform(pairing)
-    AppointmentNotif.initial_message(pairing).deliver_now
-  end
-end
--- app/jobs/sms_worker.rb
+++
@@ -1,8 +0,0 @@
-class SmsWorker < ActiveJob::Base
-  sidekiq_options retry: false
-  queue_as :default
-
-  def perform(phone_number, message)
-    SmsService.new.send_sms(phone_number, message)
-  end
-end
