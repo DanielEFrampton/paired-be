1c12e18cfcf371148fed8c2b2d450032bd7f0ce5
Becky Robran <rerobran@gmail.com>
2020-03-04 19:51:24 -0700

    adds cancelMenteePairings resolver

---
+++ app/graphql/mutations/pairings/cancel_mentee_pairing.rb
@@ -0,0 +1,30 @@
+module Mutations
+  module Pairings
+    class CancelMenteePairing < ::Mutations::BaseMutation
+      argument :id, ID, required: true
+      argument :pairer, String, required: true
+      type Types::PairingType
+
+      def resolve(attributes)
+        pairing = Pairing.find(attributes[:id])
+        notifications(pairing)
+        pairing.delete
+      end
+
+      private
+
+      def create_message(pairing)
+        name = pairing.pairee_name
+        date = pairing.date
+        time = pairing.time
+        MessageGenerator.new.cancel_notification(name, date, time)
+      end
+
+      def notifications(pairing)
+        message = create_message(pairing)
+        contact_info = pairing.pairer_contact_info
+        NotificationsWorker.perform_later(contact_info, message)
+      end
+    end
+  end
+end
