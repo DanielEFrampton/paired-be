ac396bf86cb733451672e0eb44fd1cd817bc8a85
Becky Robran <rerobran@gmail.com>
2020-03-04 21:17:30 -0700

    updates stats spec

--- spec/graphql/queries/stats/get_user_stats_spec.rb
+++ spec/graphql/queries/stats/get_user_stats_spec.rb
@@ -3,16 +3,17 @@ require 'rails_helper'
 RSpec.describe Types::QueryType do
   describe 'display users' do
     before :each do
-      user = create(:user, id: 1, name: 'Susie Q')
+      @user = create(:user, id: 1, name: 'Susie Q')
       user_1 = create(:user)
       user_2 = create(:user)
       user_3 = create(:user)
 
-      create(:pairing, pairer_id: user.id, pairee_id: user_1.id)
-      create(:pairing, pairer_id: user.id, pairee_id: user_2.id)
-      create(:pairing, pairer_id: user.id, pairee_id: user_3.id)
-      create(:pairing, pairer_id: user.id, pairee_id: user_3.id)
-      create(:pairing, pairer_id: user.id, pairee_id: user_3.id)
+      create(:pairing, pairer_id: @user.id, pairee_id: user_1.id)
+      create(:pairing, pairer_id: @user.id, pairee_id: user_1.id)
+      create(:pairing, pairer_id: @user.id, pairee_id: user_2.id)
+      create(:pairing, pairer_id: @user.id, pairee_id: user_3.id)
+      create(:pairing, pairer_id: @user.id, pairee_id: user_3.id)
+      x = create(:pairing, pairer_id: @user.id, pairee_id: nil)
     end
     it 'can find user stats' do
       result = PairedBeSchema.execute(query).as_json
@@ -21,7 +22,15 @@ RSpec.describe Types::QueryType do
       expect(result["data"]["getUserStats"]["totalMentorHours"]).to eq(2.5)
       expect(result["data"]["getUserStats"]["totalHoursMentored"]).to eq(0)
       expect(result["data"]["getUserStats"]["mentees"].count).to eq(3)
-    end 
+    end
+
+    it 'can have a mentee that is no longer in the system' do
+        user_4 = create(:user, id: 343)
+        create(:pairing, pairer_id: @user.id, pairee_id: 343)
+      result = PairedBeSchema.execute(query_2).as_json
+      result = PairedBeSchema.execute(query).as_json
+      expect(result["data"]["getUserStats"]["mentees"].count).to eq(3)
+    end
   end
 
   def query
@@ -35,6 +44,17 @@ RSpec.describe Types::QueryType do
         mentees {
           name }
         }
+    }
+    GQL
+  end
+  def query_2
+    <<~GQL
+    mutation {
+      deleteUser(
+        input:{
+          id: "343" } ) {
+        name
+      }
     }
     GQL
   end
