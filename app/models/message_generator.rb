3284b80272183a97ba5e36b7f4265f0aac170df2
Becky Robran <rerobran@gmail.com>
2020-03-04 19:52:30 -0700

    adds cancel notification methods

--- app/models/message_generator.rb
+++ app/models/message_generator.rb
@@ -5,6 +5,11 @@ class MessageGenerator
     "View it at #{paired_link}."
   end
 
+  def cancel_notification(name, date, time)
+    "#{name} has cancelled a pairing session with you from #{times[time]} " +
+    "on #{date_formatter(date)}. Keep pairing!"
+  end
+
   private
 
   def times
