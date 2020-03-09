53539cbf4be68c82f4d0fa2bd74e8add823d58d4
msimon42 <msimon@uab.edu>
2020-03-08 21:17:15 -0600

    Add reminder message to appointmentnotif mailefr

--- app/mailers/appointment_notif.rb
+++ app/mailers/appointment_notif.rb
@@ -3,4 +3,9 @@ class AppointmentNotif < ApplicationMailer
     @message = message
     mail(to: address, subject: "You have a new pairing booked!")
   end
+
+  def reminder_message(address, message)
+    @message = message
+    mail(to: address, subject: "You have a pairing in one hour!")
+  end
 end
