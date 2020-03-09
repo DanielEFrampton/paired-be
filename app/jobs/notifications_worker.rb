c1b76dd74df6aad5d2eee9353c0ae31fc7c8e198
msimon42 <msimon@uab.edu>
2020-03-08 20:16:21 -0600

    convert nil phone number to empty string

--- app/jobs/notifications_worker.rb
+++ app/jobs/notifications_worker.rb
@@ -2,9 +2,9 @@ class NotificationsWorker < ActiveJob::Base
   queue_as :default
 
   def perform(contact_info, message, type)
-    phone_number = contact_info[:phone_number]
+    phone_number = contact_info[:phone_number].to_s
     address = contact_info[:email_address]
-    SmsService.new.send_sms(phone_number, message) if phone_number
-    AppointmentNotif.send(type, address, message).deliver_now unless phone_number
+    sms = SmsService.new.send_sms(phone_number, message)
+    AppointmentNotif.send(type, address, message).deliver_now unless sms['response'] == 'success'
   end
 end
