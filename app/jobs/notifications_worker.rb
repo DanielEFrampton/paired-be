49032aada94808582c41150243658c3ded56ebb9
msimon42 <msimon@uab.edu>
2020-03-08 19:13:43 -0600

    Add type argument to notifications worker

--- app/jobs/notifications_worker.rb
+++ app/jobs/notifications_worker.rb
@@ -1,10 +1,10 @@
 class NotificationsWorker < ActiveJob::Base
   queue_as :default
 
-  def perform(contact_info, message)
+  def perform(contact_info, message, type)
     phone_number = contact_info[:phone_number]
     address = contact_info[:email_address]
     SmsService.new.send_sms(phone_number, message) if phone_number
-    AppointmentNotif.initial_message(address, message).deliver_now unless phone_number
+    AppointmentNotif.send(type, address, message).deliver_now unless phone_number
   end
 end
