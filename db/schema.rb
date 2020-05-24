5eb2b88f2b7d79a7e90df06fe073588e8a4d3606
Becky Robran <rerobran@gmail.com>
2020-05-24 15:51:10 -0600

    updates schema with opt in status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_175936) do
+ActiveRecord::Schema.define(version: 2020_05_24_212834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_175936) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "firebase_id"
     t.integer "mod"
+    t.boolean "rock_opt_in", default: false
   end
 
   add_foreign_key "interests", "users"
