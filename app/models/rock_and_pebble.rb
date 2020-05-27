f9be03ffdae43fdc823a558887e4f2a88f0a8a61
Becky Robran <rerobran@gmail.com>
2020-05-27 11:53:36 -0600

    adds rock and pebble to factory bot

--- app/models/rock_and_pebble.rb
+++ app/models/rock_and_pebble.rb
@@ -1,5 +1,13 @@
 class RockAndPebble < ApplicationRecord
     belongs_to :rock, class_name: 'User'
     belongs_to :pebble, class_name: 'User'
-end
 
+    def rock_pebble_info(reason = nil )
+      {rock_name: rock.name,
+       pebble_name: pebble.name,
+       pebble_email: pebble.email,
+       rock_email: rock.email,
+       reason: reason
+      }
+    end
+end
