c18c97044e7c8563805cf5ada9b0676d63a156c3
Becky Robran <rerobran@gmail.com>
2020-05-27 14:32:35 -0600

    adds rock and pebble fields for query returns

--- app/graphql/types/rock_and_pebble_type.rb
+++ app/graphql/types/rock_and_pebble_type.rb
@@ -4,5 +4,17 @@ module Types
         field :rock, Types::UserType, null: false
         field :pebble, Types::UserType, null: false
         field :active, Boolean, null: false
+        field :my_rocks, [Types::UserType], null: true
+        field :my_pebbles, [Types::UserType], null: true
+
+        def my_rocks
+          user = User.find(object.pebble_id)
+          user.rocks
+        end
+
+        def my_pebbles
+          user = User.find(object.rock_id)
+          user.pebbles
+        end
     end
 end
