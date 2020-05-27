283de1f177f500a929788d34fd81088df36c1461
Becky Robran <rerobran@gmail.com>
2020-05-27 11:54:55 -0600

    adds testing for delete_rock_and_pebble mutation

---
+++ spec/graphql/mutations/rock_and_pebble/delete_rock_pebble_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'delete rock and pebble relationship', type: :request do
+  describe 'resolve' do
+    before :each do
+      @rock_and_pebble = create :rock_and_pebble
+      @query = <<~GQL
+                mutation {
+                  rock_and_pebble: deleteRockPebbleRelationship(
+                    input: {
+                      id: #{@rock_and_pebble.id}
+                      reason: "I'm too busy right now. Sorry."
+                    }
+                  ){
+                    rock {id}
+                    pebble {id}
+                  }
+                }
+              GQL
+      @rock = @rock_and_pebble.rock
+      @pebble = @rock_and_pebble.pebble
+    end
+
+    it 'deletes the pairing' do
+      expect(RockAndPebble.count).to eq(1)
+      post '/graphql', params: {query: @query}
+      result = JSON.parse(response.body)['data']['rock_and_pebble']
+      expect(result['rock']['id']).to eq(@rock.id.to_s)
+      expect(result['pebble']['id']).to eq(@pebble.id.to_s)
+      expect(RockAndPebble.count).to eq(0)
+    end
+  end
+end
