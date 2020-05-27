12d8b25efb7bec053aa60c1c9036834383c7f421
Becky Robran <rerobran@gmail.com>
2020-05-27 11:52:06 -0600

    adds delete_rock_pebble mutation

---
+++ app/graphql/mutations/rock_and_pebbles/delete_rock_pebble_relationship.rb
@@ -0,0 +1,25 @@
+module Mutations
+  module RockAndPebbles
+    class DeleteRockPebbleRelationship < ::Mutations::BaseMutation
+      argument :id, ID, required: true
+      argument :reason, String, required: true
+      type Types::RockAndPebbleType
+
+      def resolve(attributes)
+        rock_and_pebble = RockAndPebble.find(attributes[:id])
+        send_pebble_email(rock_and_pebble, attributes)
+
+        rock_and_pebble.destroy 
+      end
+
+      private
+
+      def send_pebble_email(rock_and_pebble, attributes)
+        reason = attributes[:reason]
+        info = rock_and_pebble.rock_pebble_info
+
+        NotificationsWorker.rock_pebble_message(info, :rock_denied_message)
+      end
+    end
+  end
+end
