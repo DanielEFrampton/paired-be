97012a7cb8030630e131fc60e1344c82cbbceae6
Becky Robran <rerobran@gmail.com>
2020-05-27 14:08:46 -0600

    adds reason to rock_and_pebble info

--- app/graphql/mutations/rock_and_pebbles/delete_rock_pebble_relationship.rb
+++ app/graphql/mutations/rock_and_pebbles/delete_rock_pebble_relationship.rb
@@ -9,14 +9,14 @@ module Mutations
         rock_and_pebble = RockAndPebble.find(attributes[:id])
         send_pebble_email(rock_and_pebble, attributes)
 
-        rock_and_pebble.destroy 
+        rock_and_pebble.destroy
       end
 
       private
 
       def send_pebble_email(rock_and_pebble, attributes)
         reason = attributes[:reason]
-        info = rock_and_pebble.rock_pebble_info
+        info = rock_and_pebble.rock_pebble_info(reason)
 
         NotificationsWorker.rock_pebble_message(info, :rock_denied_message)
       end
