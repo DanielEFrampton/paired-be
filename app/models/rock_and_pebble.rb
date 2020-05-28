df582b5878a393422571c827c497bb169c70dc58
Becky Robran <rerobran@gmail.com>
2020-05-27 20:57:20 -0600

    updates helper contact info to not include a reason

--- app/models/rock_and_pebble.rb
+++ app/models/rock_and_pebble.rb
@@ -2,12 +2,11 @@ class RockAndPebble < ApplicationRecord
     belongs_to :rock, class_name: 'User'
     belongs_to :pebble, class_name: 'User'
 
-    def rock_pebble_info(reason = nil )
+    def rock_pebble_info
       {rock_name: rock.name,
        pebble_name: pebble.name,
        pebble_email: pebble.email,
-       rock_email: rock.email,
-       reason: reason
+       rock_email: rock.email
       }
     end
 end
