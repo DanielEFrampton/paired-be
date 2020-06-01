b5d804a7eba3ac756c464c372b2b4e36f2a0486a
msimon42 <msimon@uab.edu>
2020-06-01 15:37:22 -0600

    Add args

--- app/graphql/mutations/rock_and_pebbles/create_rock_pebble_relationship.rb
+++ app/graphql/mutations/rock_and_pebbles/create_rock_pebble_relationship.rb
@@ -1,8 +1,12 @@
 module Mutations
   module RockAndPebbles
     class CreateRockPebbleRelationship < ::Mutation::BaseMutation
+      argument :rock_id, required: true
+      argument :pebble_id, required: true
+      type Types::RockAndPebbleType
+
 
 
     end
   end
-end       
+end
