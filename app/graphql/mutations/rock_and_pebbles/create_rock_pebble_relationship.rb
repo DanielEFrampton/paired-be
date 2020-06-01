18451fdeac6836c83a87251746fa58806367b545
msimon42 <msimon@uab.edu>
2020-06-01 16:24:14 -0600

    Add mailer

--- app/graphql/mutations/rock_and_pebbles/create_rock_pebble_relationship.rb
+++ app/graphql/mutations/rock_and_pebbles/create_rock_pebble_relationship.rb
@@ -10,6 +10,15 @@ module Mutations
           rock_id: attributes[:rock_id],
           pebble_id: attributes[:pebble_id],
         )
+
+        send_confirmation_email(new_rp)      
+      end
+
+      private
+
+      def send_confirmation_email(new_rp)
+        info = new_rp.rock_pebble_info
+        NotificationsWorker.rock_pebble_message(info, nil, :confirmation_message)
       end
     end
   end
