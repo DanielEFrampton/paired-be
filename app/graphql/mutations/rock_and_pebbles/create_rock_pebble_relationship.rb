dbc37df654cd17e7329b7630a0dd0634409aa5e8
msimon42 <msimon@uab.edu>
2020-06-01 15:41:41 -0600

    Instanitate rock and pebble

--- app/graphql/mutations/rock_and_pebbles/create_rock_pebble_relationship.rb
+++ app/graphql/mutations/rock_and_pebbles/create_rock_pebble_relationship.rb
@@ -5,8 +5,12 @@ module Mutations
       argument :pebble_id, required: true
       type Types::RockAndPebbleType
 
-
-
+      def resolve(attributes)
+        new_rp = RockAndPebble.create(
+          rock_id: attributes[:rock_id],
+          pebble_id: attributes[:pebble_id],
+        )
+      end
     end
   end
 end
