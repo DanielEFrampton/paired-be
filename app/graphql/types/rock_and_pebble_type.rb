f800b0e2758cf213d36ff2b17b1d352e03f66b4c
Linda Le <yueling1222@gmail.com>
2020-06-01 21:38:15 -0600

    Remove unnecessary fields from RockAndPebble

--- app/graphql/types/rock_and_pebble_type.rb
+++ app/graphql/types/rock_and_pebble_type.rb
@@ -1,21 +1,8 @@
 module Types
   class RockAndPebbleType < Types::BaseObject
       field :id, ID, null: false
-      field :user, Types::UserType, null: true
-      # field :pebble, Types::UserType, null: true
+      field :rock, Types::UserType, null: true
+      field :pebble, Types::UserType, null: true
       field :active, Boolean, null: false
-      field :my_rocks, [Types::UserType], null: true
-      field :my_pebbles, [Types::UserType], null: true
-
-
-    def my_rocks
-      user = User.find(object.pebble_id)
-      user.rocks
-    end
-
-    def my_pebbles
-      user = User.find(object.rock_id)
-      user.pebbles
-    end
   end
 end
