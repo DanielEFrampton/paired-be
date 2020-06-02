9d986bbe4122ce7e8b573512a9f1dbd783735623
Linda Le <yueling1222@gmail.com>
2020-06-01 21:47:25 -0600

    Add deleted methods back in

--- app/graphql/types/rock_and_pebble_type.rb
+++ app/graphql/types/rock_and_pebble_type.rb
@@ -6,5 +6,15 @@ module Types
       field :active, Boolean, null: false
       field :my_rocks, [Types::UserType], null: true
       field :my_pebbles, [Types::UserType], null: true
+
+  def my_rocks
+    user = User.find(object.pebble_id)
+    user.rocks
+  end
+
+  def my_pebbles
+    user = User.find(object.rock_id)
+    user.pebbles
+  end
   end
 end
