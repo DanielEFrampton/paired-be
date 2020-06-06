832975b274c834d85f8cf19522e044c0ea52a0d0
msimon42 <msimon@uab.edu>
2020-06-05 23:44:40 -0600

    Add query

--- spec/graphql/mutations/rock_and_pebble/create_rock_and_pebble_spec.rb
+++ spec/graphql/mutations/rock_and_pebble/create_rock_and_pebble_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe 'create rp relationship', type: :request do
     before :each do
       @user_1 = create :user
       @user_2 = create :user
-      
+
       @query = <<~GQL
                 mutation {
-                  rock_and_pebble: CreateRockPebbleRelationship
+                  rock_and_pebble: CreateRockPebbleRelationship(
+                    input: {
+                      rock_id: #{@user_1.id}
+                      pebble_id: #{@user_2.id}
+                    }
+                  )
                 }
-
-
-
-
-
 
               GQL
     end
