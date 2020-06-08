954ccd61f6758885d25876269a3a9d30e731229b
Becky Robran <rerobran@gmail.com>
2020-06-07 19:22:19 -0600

    adds test to check for filter query find rocks

---
+++ spec/graphql/mutations/rock_and_pebble/find_available_rocks_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe 'display rocks' do
+    it 'can query all available rocks' do
+
+        #user_1 and user_2 should be returned in the query
+        user = create(:user, id: 23, mod: 2, program: "BE",  rock_opt_in: true)
+        user_1 = create(:user, id: 24, mod: 2, program: "BE", rock_opt_in: true)
+        user_2 = create(:user, id: 25, mod: 2, program: "BE", rock_opt_in: true)
+        user_3 = create(:user, mod: 2, program: "BE")
+
+        user_4 = create(:user, mod: 3, program: "BE", rock_opt_in: true)
+
+
+        user_5 = create(:user, mod: 4, program: "FE", rock_opt_in: true)
+        user_6 = create(:user, mod: 3, program: "FE", rock_opt_in: true)
+        user_7 = create(:user, mod: 4, program: "FE")
+
+        RockAndPebble.create(rock_id: 23, pebble_id: user_5.id, active: true)
+        RockAndPebble.create(rock_id: 23, pebble_id: user_6.id, active: true)
+
+        RockAndPebble.create(rock_id: 24, pebble_id: user_7.id, active: false)
+        RockAndPebble.create(rock_id: 24, pebble_id: user_5.id, active: true)
+
+      result = PairedBeSchema.execute(query).as_json
+
+      expect(result["data"]["findAvailableRocks"].count).to eq(2)
+      expect(result["data"]["findAvailableRocks"][0]["name"]).to eq(user_1.name)
+      expect(result["data"]["findAvailableRocks"][1]["name"]).to eq(user_2.name)
+    end
+  end
+
+  def query
+    <<~GQL
+    {
+        findAvailableRocks(filter: {program: "BE", module: 2}) {
+
+            name
+            module
+            program
+            id
+            pronouns
+            skills
+            slack
+            image
+        }
+      }
+    GQL
+  end
+end
