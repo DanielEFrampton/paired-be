f4ebd2e86c9572026967516253ec37aa57cc9e9b
msimon42 <msimon@uab.edu>
2020-06-11 12:04:48 -0600

    Update from address

--- app/mailers/appointment_notif.rb
+++ app/mailers/appointment_notif.rb
@@ -1,16 +1,16 @@
 class AppointmentNotif < ApplicationMailer
   def initial_message(address, message)
     @message = message
-    mail(to: address, subject: "You have a new pairing booked!")
+    mail_obj = mail(to: address, from: 'bookings@paired.tech', subject: "You have a new pairing booked!")
   end
 
   def reminder_message(address, message)
     @message = message
-    mail(to: address, subject: "You have a pairing today!")
+    mail(to: address, from: 'bookings@paired.tech', subject: "You have a pairing today!")
   end
 
   def cancel_message(address, message)
     @message = message
-    mail(to: address, subject: "Your pairing has been canceled. :(")
+    mail(to: address, from: 'bookings@paired.tech', subject: "Your pairing has been canceled. :(")
   end
 end
