ad908d4118f864d3abea6aed7c579bb202f65f96
msimon42 <msimon@uab.edu>
2020-06-11 11:59:56 -0600

    Add method that records outgoing messages

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'no_reply@paired.tech'
   layout 'mailer'
+
+  def record_outgoing_message(recip_address, response_status, response_body, message_type)
+    user = User.find_by(email: recip_address)
+    OutgoingEmailCommunication.create(
+      user: user,
+      response_status: response_status,
+      response_body: response_body,
+      message_type: message_type,
+      created_at: Time.now
+    )
+  end
 end
