7e7e23b6840028c086b37da7aaf827c36a3ddd29
msimon42 <msimon@uab.edu>
2020-06-11 16:11:10 -0600

    Add recording method to each mailer

--- app/mailers/appointment_notif.rb
+++ app/mailers/appointment_notif.rb
@@ -2,15 +2,18 @@ class AppointmentNotif < ApplicationMailer
   def initial_message(address, message)
     @message = message
     mail_obj = mail(to: address, from: 'bookings@paired.tech', subject: "You have a new pairing booked!")
+    record_outgoing_message(address, mail_obj, 0)
   end
 
   def reminder_message(address, message)
     @message = message
-    mail(to: address, from: 'bookings@paired.tech', subject: "You have a pairing today!")
+    mail_obj = mail(to: address, from: 'bookings@paired.tech', subject: "You have a pairing today!")
+    record_outgoing_message(address, mail_obj, 1)
   end
 
   def cancel_message(address, message)
     @message = message
-    mail(to: address, from: 'bookings@paired.tech', subject: "Your pairing has been canceled. :(")
+    mail_obj = mail(to: address, from: 'bookings@paired.tech', subject: "Your pairing has been canceled. :(")
+    record_outgoing_message(address, mail_obj, 2)
   end
 end
