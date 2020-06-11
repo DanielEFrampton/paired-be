620b9c59864e61da4ceb38d15a4328f8a7d8b92a
msimon42 <msimon@uab.edu>
2020-06-11 16:11:36 -0600

    Move sendgrid api call

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,15 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'no_reply@paired.tech'
   layout 'mailer'
 
-  def record_outgoing_message(recip_address, response_status, response_body, message_type)
+  def record_outgoing_message(recip_address, mail_obj, message_type)
     user = User.find_by(email: recip_address)
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+
     OutgoingEmailCommunication.create(
       user: user,
-      response_status: response_status,
-      response_body: response_body,
+      response_status: response.status_code,
+      response_body: response.body,
       message_type: message_type,
       created_at: Time.now
     )
