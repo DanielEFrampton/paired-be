bf8f81a982c2b040a5b3bc25a744a727052105eb
msimon42 <msimon@uab.edu>
2020-06-11 11:48:55 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_011714) do
+ActiveRecord::Schema.define(version: 2020_06_11_173138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2020_06_08_011714) do
     t.string "name"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_interests_on_user_id"
+  end
+
+  create_table "outgoing_email_communications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "response_body"
+    t.string "response_status"
+    t.string "message_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_outgoing_email_communications_on_user_id"
   end
 
   create_table "pairings", force: :cascade do |t|
@@ -65,5 +75,6 @@ ActiveRecord::Schema.define(version: 2020_06_08_011714) do
   end
 
   add_foreign_key "interests", "users"
+  add_foreign_key "outgoing_email_communications", "users"
   add_foreign_key "skills", "users"
 end
