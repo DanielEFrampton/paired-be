c40e3903b256eedef8fb6fb2524df19dc0bb53a1
msimon42 <msimon@uab.edu>
2020-06-11 16:30:20 -0600

    Add explicit conversion to migration

--- db/migrate/20200611222150_change_type_to_integer.rb
+++ db/migrate/20200611222150_change_type_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeTypeToInteger < ActiveRecord::Migration[6.0]
   def change
-    change_column :outgoing_email_communications, :message_type, :integer
+    change_column :outgoing_email_communications, :message_type, :integer, using: 'message_type::integer'
   end
 end
