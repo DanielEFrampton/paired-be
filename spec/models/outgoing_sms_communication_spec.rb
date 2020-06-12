f3925a93b5fb1f73e6d9cc44eca9cd4ea4703f6a
msimon42 <msimon@uab.edu>
2020-06-12 16:53:57 -0600

    Update again for reflection again

--- spec/models/outgoing_sms_communication_spec.rb
+++ spec/models/outgoing_sms_communication_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe OutgoingSmsCommunication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to :user}
+  end
+
+  describe 'methods' do
+    it 'self.record' do
+      user = create :user
+      status = 'success'
+      msg_type = :initial_message
+
+      OutgoingSmsCommunication.record(user.email, status, msg_type)
+
+      test_obj = OutgoingSmsCommunication.last
+      expect(test_obj.user).to eq(user)
+      expect(test_obj.status).to eq(status)
+      expect(test_obj.message_type).to eq(msg_type.to_s)
+    end
+
+    it 'self.type_map' do
+      type_map = OutgoingSmsCommunication.type_map
+      expect(type_map[:initial_message]).to eq(0)
+      expect(type_map[:cancellation_message]).to eq(1)
+      expect(type_map[:reminder_message]).to eq(2)
+    end
+  end
+
 end
