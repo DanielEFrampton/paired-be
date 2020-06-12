de81d7544673ff7f6d7a10724d27769296af3658
msimon42 <msimon@uab.edu>
2020-06-12 16:40:22 -0600

    Update test to reflect changes

--- spec/services/sms_service_spec.rb
+++ spec/services/sms_service_spec.rb
@@ -5,11 +5,13 @@ feature SmsService do
     feature 'send_sms' do
       scenario 'sends request to SMS microserivce and returns success or failure as JSON', :vcr do
         phone_number = ENV['TEST_PHONE_NUMBER']
+        email_address = ENV['TEST_EMAIL_ADDRESS']
+        contact_info = {phone_number: phone_number, email_address: email_address}
         message = "Matt Simon has booked a pairing session with you " +
                   "from 4:10-4:40pm on Thursday, October 22nd using " +
                   "Paired! View it at https://www.paired.tech."
 
-        response_1 = SmsService.new.send_sms(phone_number, message)
+        response_1 = SmsService.new.send_sms(contact_info, message, :initial_message)
 
         expect(response_1.class).to eq(Hash)
         expect(response_1['response']).to eq('success')
