90e542330940126dca422cd641c7de5ca2f3235c
msimon42 <msimon@uab.edu>
2020-06-12 16:03:23 -0600

    Change argument to contact info

--- app/jobs/notifications_worker.rb
+++ app/jobs/notifications_worker.rb
@@ -2,9 +2,8 @@ class NotificationsWorker < ActiveJob::Base
   queue_as :default
 
   def perform(contact_info, message, type)
-    phone_number = contact_info[:phone_number].to_s
     address = contact_info[:email_address]
-    sms = SmsService.new.send_sms(phone_number, message, type)
+    sms = SmsService.new.send_sms(contact_info, message, type)
     AppointmentNotif.send(type, address, message).deliver_now unless sms['response'] == 'success'
   end
 
