495f2356c10df75969e3c502bdeff72990f8127c
Linda Le <yueling1222@gmail.com>
2020-06-12 13:36:47 -0600

    Update test to check rock accepted email is sent

--- spec/graphql/mutations/rock_and_pebble/activate_rock_pebble_spec.rb
+++ spec/graphql/mutations/rock_and_pebble/activate_rock_pebble_spec.rb
@@ -6,30 +6,51 @@ RSpec.describe 'activate rock and pebble relationship', type: :request do
             @user_2 = create :user
             @user_1.pebbles << @user_2
             @relationship = RockAndPebble.first
-            @user_3 = create :user
-            @user_4 = create :user
-            @user_3.pebbles << @user_4
-            @other_relationship = RockAndPebble.last
     end
 
     describe '.resolve' do
-        it 'switches the active status of the relationship to true' do
-            expect(RockAndPebble.count).to eq(2)
+        it 'switches the active status of the relationship to true and send rock accepted email' do
+            expect(RockAndPebble.count).to eq(1)
+            expect(@relationship.active).to eq(false)
             post '/graphql', params: {query: query}
-            expect(@relationship.active).to eq(true)
-            expect(@other_relationship.active).to eq(false)
-        end
+            result = JSON.parse(response.body)
+            expect(result['data']['rock_and_pebble']['id']).to eq(@relationship.id.to_s)
+            expect(result['data']['rock_and_pebble']['active']).to eq(true)
+            expect(result['data']['rock_and_pebble']['rock']['id']).to eq(@user_1.id.to_s)
+            expect(result['data']['rock_and_pebble']['pebble']['id']).to eq(@user_2.id.to_s)
+            expect do
+            post '/graphql', params: {query: query}
+            end.
+            to change { ActionMailer::Base.deliveries.count }.by(1)
+          end
     end
         def query
             <<~GQL
                 mutation {
-                    rock_and_pebble: activateRockAndPebble(
+                    rock_and_pebble: activateRockPebbleRelationship(
                     input: {
                     id: #{@relationship.id}
                     }
                     ){
-                    rock{id}
-                    pebble{id}
+                      id
+                      rock {
+                        id
+                        name
+                        pronouns
+                        program
+                        module
+                        slack
+                      }
+                      pebble {
+                        id
+                        name
+                        pronouns
+                        program
+                        module
+                        slack
+                      }
+                      active
+
                 }
                 }
             GQL
