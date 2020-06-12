cadfcf08c02bcd5576e5379534f663141b52c66c
msimon42 <msimon@uab.edu>
2020-06-12 17:17:52 -0600

    Add relationship test to o_e_c

--- spec/models/outgoing_email_communication_spec.rb
+++ spec/models/outgoing_email_communication_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe OutgoingEmailCommunication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to :user}
+  end
 end
