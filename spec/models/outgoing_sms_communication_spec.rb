67c1f81e16cca9c9cb6044266967ffc13d193569
msimon42 <msimon@uab.edu>
2020-06-23 16:50:48 -0600

    Update tests to reflect changes

--- spec/models/outgoing_sms_communication_spec.rb
+++ spec/models/outgoing_sms_communication_spec.rb
@@ -6,25 +6,17 @@ RSpec.describe OutgoingSmsCommunication, type: :model do
   end
 
   describe 'methods' do
-    it 'self.record' do
+    it 'self.create' do
       user = create :user
       status = 'success'
       msg_type = :initial_message
 
-      OutgoingSmsCommunication.record(user.email, status, msg_type)
+      OutgoingSmsCommunication.create(user.email, status, msg_type)
 
       test_obj = OutgoingSmsCommunication.last
       expect(test_obj.user).to eq(user)
       expect(test_obj.status).to eq(status)
       expect(test_obj.message_type).to eq(msg_type.to_s)
     end
-
-    it 'self.type_map' do
-      type_map = OutgoingSmsCommunication.type_map
-      expect(type_map[:initial_message]).to eq(0)
-      expect(type_map[:cancellation_message]).to eq(1)
-      expect(type_map[:reminder_message]).to eq(2)
-    end
   end
-
 end
