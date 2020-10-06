c58d1ea608df7b8681cd4751fc8cfde053a37c33
Becky Robran <rerobran@gmail.com>
2020-10-05 21:46:18 -0600

    adds test for discontinue mutation

--- spec/graphql/mutations/rock_and_pebble/discontinue_rock_pebble_spec.rb
+++ spec/graphql/mutations/rock_and_pebble/discontinue_rock_pebble_spec.rb
@@ -1,51 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe 'discontinue rock and pebble relationship', type: :request do
-  describe 'resolve' do
+    describe 'resolve', :vcr do
     before :each do
       @user_1 = create :user
       @user_2 = create :user
       @user_3 = create :user
+      @user_4 = create :user
       @user_1.pebbles << [@user_2, @user_3]
-      @rock_and_pebble = RockAndPebble.first
-      @query = <<~GQL
+      RockAndPebble.last.update(active: true)
+      RockAndPebble.first.update(active: true)
+      @rock_and_pebble = RockAndPebble.last
+      
+      @query1 = <<~GQL
                 mutation {
                   rock_and_pebble: discontinueRockPebbleRelationship(
                     input: {
                       id: #{@rock_and_pebble.id}
                       reason: "I'm too busy to keep mentoring."
-                      user_id: #{@user_1.id}
+                      userId: #{@user_1.id}
                     }
                   ){
-                    my rocks {
+
+                    myRocks {
                                 id
                                 name
-                                pronouns
-                                program
-                                module
                                 slack
                     }
                     myPebbles {
-                                id
+                                id 
                                 name
-                                pronouns
-                                program
-                                module
-                                slack }
-
+                                slack
+                              }
                   }
                 }
               GQL
+
       @rock = @rock_and_pebble.rock
     end
 
     it 'deactivates the rock and pebble relationship' do
-      expect(RockAndPebble.count).to eq(2)
-      post '/graphql', params: {query: @query}
-      result = JSON.parse(response.body)['data']['rock_and_pebble'] 
-      expect(result['rock']['id']).to eq(@rock.id.to_s)
-      expect(result['myPebbles'].first["name"]).to eq("#{@user_3.name}")
-      expect(RockAndPebble.count).to eq(1)
+      expect(@rock_and_pebble.reload.active).to eq(true)
+
+      post '/graphql', params: {query: @query1}
+      result = JSON.parse(response.body)["data"]["rock_and_pebble"]
+
+      expect(result['myRocks']).to be_empty
+      expect(result['myPebbles'].first['name']).to eq("#{@user_2.name}")
+      expect(@rock_and_pebble.reload.active).to eq(false)
     end
   end
 end
