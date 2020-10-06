1c4443a78054f551a2cecde38635857f562b1a32
Becky Robran <rerobran@gmail.com>
2020-10-05 21:45:41 -0600

    adds test for discontinue mutation

--- app/graphql/mutations/rock_and_pebbles/discontinue_rock_pebble_relationship.rb
+++ app/graphql/mutations/rock_and_pebbles/discontinue_rock_pebble_relationship.rb
@@ -3,31 +3,32 @@ module Mutations
       class DiscontinueRockPebbleRelationship < ::Mutations::BaseMutation
         argument :id, ID, required: true
         argument :reason, String, required: true
-        argument :relationship, ID, required: true
+        argument :userId, ID, required: true
         type Types::RockAndPebbleType
-  
+
         def resolve(attributes)
-          rock_and_pebble = RockAndPebble.find(attributes[:relationship])
+          # require 'pry', binding.pry
+          rock_and_pebble = RockAndPebble.find(attributes[:id])
           send_pebble_email(rock_and_pebble, attributes)
-  
+
           rock_and_pebble.update(active: false)
+          rock_and_pebble
         end
-  
+
         private
-  
+
         def send_pebble_email(rock_and_pebble, attributes)
-          user = User.find(attributes[:id])
+          user = User.find(attributes[:user_id])
           if user.id == rock_and_pebble.rock_id
             reason = attributes[:reason]
             info = rock_and_pebble.rock_pebble_info
             NotificationsWorker.rock_pebble_message(info, reason, :rock_denied_message)
-          else 
+          else
             reason = attributes[:reason]
             info = rock_and_pebble.rock_pebble_info
             NotificationsWorker.rock_pebble_message(info, reason, :rock_denied_message)
-          end 
+          end
         end
       end
     end
   end
-  
