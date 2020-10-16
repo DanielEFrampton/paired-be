08e29d834a5fad8bd05efc2ccc238c27c64c27ad
Becky Robran <rerobran@gmail.com>
2020-10-15 19:53:27 -0600

    adds new arguments to mutation

--- app/graphql/mutations/rock_and_pebbles/decline_rock_pebble_relationship.rb
+++ app/graphql/mutations/rock_and_pebbles/decline_rock_pebble_relationship.rb
@@ -1,12 +1,13 @@
 module Mutations
   module RockAndPebbles
     class DeclineRockPebbleRelationship < ::Mutations::BaseMutation
-      argument :id, ID, required: true
+      argument :rock_id, ID, required: true
+      argument :pebble_id, ID, required: true
       argument :reason, String, required: true
       type Types::RockAndPebbleType
 
       def resolve(attributes)
-        rock_and_pebble = RockAndPebble.find(attributes[:id])
+        rock_and_pebble = RockAndPebble.where(rock_id: attributes[:rock_id]).where(pebble_id: attributes[pebble_id]).where(pending: false)
         send_pebble_email(rock_and_pebble, attributes)
 
         rock_and_pebble.destroy
